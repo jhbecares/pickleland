22ecbd6de1fffb75521455656cc2cdd8130489e9
Jenny Hernández <jhdez.becares@gmail.com>
2017-07-10 21:33:36 +0200

    SE MUEVE el monigotegit status!

---
+++ pepichurro/Assets/Scripts/MonigoteMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonigoteMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * 10f * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * 10f * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * 10f * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * 10f * Time.deltaTime);
+        }
+        else
+        {
+
+        }
+	}
+}
