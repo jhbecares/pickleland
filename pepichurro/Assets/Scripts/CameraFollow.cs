513066a1da20d9193bddd2e322ed2ed4259afe49
Jenny Hernández <jhdez.becares@gmail.com>
2017-07-10 22:24:03 +0200

    La camara sigue al muñeco

---
+++ pepichurro/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Transform monigote;
+
+	// Use this for initialization
+    void Start()
+    {
+        monigote = GameObject.Find("Monigote").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerpos = monigote.position;
+        playerpos.z = transform.position.z;
+        transform.position = playerpos;
+	}
+}
