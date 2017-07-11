37b1b044f8286889f9e2a40af795be8a56830e82
Viktor Jacynycz <viktor@jacynycz.es>
2017-07-11 12:45:03 +0200

    animaciones

--- pepichurro/Assets/Scripts/MonigoteMove.cs
+++ pepichurro/Assets/Scripts/MonigoteMove.cs
@@ -5,31 +5,33 @@ using UnityEngine;
 public class MonigoteMove : MonoBehaviour {
 
     public static float changeSpeed = 10f; // constante de cambio de velocidad del movimiento
+    private Animator animator;
 
 	// Use this for initialization
 	void Start () {
-		
+        animator = GetComponent<Animator>();
 	}
-
 
 	// Update is called once per frame
 	void Update () {
-
+        animator.SetBool("walking", false);
         if (Input.GetKey(KeyCode.A))
         {
+            animator.SetBool("walking", true);
+            animator.SetBool("backwards", true);
             transform.Translate(Vector3.left * changeSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D))
         {
+            animator.SetBool("walking", true);
+            animator.SetBool("backwards", false);
             transform.Translate(Vector3.right * changeSpeed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.down * changeSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.W))
+        else if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.up * changeSpeed * Time.deltaTime);
         }
-	}
+
+        
+    }
 }
