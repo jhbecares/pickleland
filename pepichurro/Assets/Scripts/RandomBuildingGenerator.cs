10264c2d5daa4aa736c1749acd6e9648151a29aa
Viktor Jacynycz <viktor@jacynycz.es>
2017-07-13 13:03:11 +0200

    mejora mapa, cubos de basura y coliders

--- pepichurro/Assets/Scripts/RandomBuildingGenerator.cs
+++ pepichurro/Assets/Scripts/RandomBuildingGenerator.cs
@@ -8,10 +8,20 @@ public class RandomBuildingGenerator : MonoBehaviour
     public GameObject farolaPrefab;
     public GameObject wreckedBuildingPrefab;
     public GameObject buildingPrefab;
+    public GameObject fondo;
+    public GameObject trashcan;
+    public GameObject trashcan2;
 
     // Use this for initialization
     void Start()
     {
+        for (int i = 0; i < 10; i++)
+        {
+            var enemy = (GameObject)Instantiate(
+            fondo,
+            new Vector3(81.8f * (i - 1f), 8.9f, 10f),
+            new Quaternion());
+        }
         Vector3 initial = new Vector3(-45, -3, 0);
         for (int i = 0; i < 1000; i++)
         {
@@ -23,10 +33,11 @@ public class RandomBuildingGenerator : MonoBehaviour
         }
         for (int i = 0; i < 1000; i++)
         {
-            Vector3 pos = new Vector3(initial.x + 45 * (i + 1), 2.17f, -0.3f);
+
             int random = Random.Range(0, 2);
             if (random == 0)
             {
+                Vector3 pos = new Vector3(initial.x + 47 * (i + 1), -1.21f, -1f);
                 var enemy = (GameObject)Instantiate(
                 wreckedBuildingPrefab,
                 pos,
@@ -34,16 +45,33 @@ public class RandomBuildingGenerator : MonoBehaviour
             }
             else
             {
+                Vector3 pos = new Vector3(initial.x + 47 * (i + 1), 1.2f, 3f);
                 var enemy = (GameObject)Instantiate(
                 buildingPrefab,
                 pos,
                 new Quaternion());
             }
         }
-    }
-        // Update is called once per frame
-        void Update()
-        {
 
+        for (int i = 0; i < 1000; i++)
+        {
+            int random = Random.Range(0, 2);
+            if (random == 0)
+            {
+                Vector3 pos = new Vector3(initial.x + 37 * (i + 1), -3.89f, 0);
+                var enemy = (GameObject)Instantiate(
+                trashcan,
+                pos,
+                new Quaternion());
+            }
+            else
+            {
+                Vector3 pos = new Vector3(initial.x + 37 * (i + 1), -3.6f, -0.6f);
+                var enemy = (GameObject)Instantiate(
+                trashcan2,
+                pos,
+                new Quaternion());
+            }
         }
+    }
 }
